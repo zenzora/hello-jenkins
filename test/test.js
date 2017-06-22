6490c46f303a7f09268ae7cc84d449d6afa76734
zenzora <jake@zenzora.com>
2017-06-22 18:41:03 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
 
 describe('GET /', function() {
   it('respond with hello jenkins', function(done) {
-    request(app).get('/').expect('hello jenkins', done);
+    request(app).get('/').expect('hello jenkins, again!', done);
   });
 });
