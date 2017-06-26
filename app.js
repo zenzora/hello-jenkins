b39fe97e63205b88fc4a9872d4823114d85431a1
zenzora <jake@zenzora.com>
2017-06-26 12:31:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
   res.send('hello jenkins, again and again!');
 });
 app.get('/new', function (req, res) {
-  res.send('wow a new page!');
+  res.send('wow a newer page!');
 });
  
 app.listen(process.env.PORT || 5000);
